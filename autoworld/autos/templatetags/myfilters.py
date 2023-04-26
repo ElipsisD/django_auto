02d8de4098ace5fb8d3f0a47d90a38062155335f
Dmitry Boyko <da-boyko@mail.ru>
2023-04-26 20:24:15 +0700

    add template filter

---
+++ autoworld/autos/templatetags/myfilters.py
@@ -0,0 +1,10 @@
+import locale
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def thousands(value):
+    """Разделение числа на разряды"""
+    return locale.format_string('%d', value, grouping=True)
