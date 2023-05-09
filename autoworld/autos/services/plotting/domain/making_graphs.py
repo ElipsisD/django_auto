00290906194075a7af0a6e0f51d7589653c230a1
Dmitry Boyko <da-boyko@mail.ru>
2023-05-09 20:14:28 +0700

    ref offset_formats

--- autoworld/autos/services/plotting/domain/making_graphs.py
+++ autoworld/autos/services/plotting/domain/making_graphs.py
@@ -29,6 +29,8 @@ def make_spare_fig(spare: str, data: SpareRequestsData) -> str:
     ax.xaxis.set_major_formatter(mdates.ConciseDateFormatter(ax.xaxis.get_major_locator()))
 
     locator = mdates.AutoDateLocator()
+    ax.xaxis.set_major_locator(locator)
+
     formatter = mdates.ConciseDateFormatter(locator)
     formatter.formats = ['%y',
                          '%b',
@@ -38,12 +40,9 @@ def make_spare_fig(spare: str, data: SpareRequestsData) -> str:
                          '%S.%f', ]
     formatter.zero_formats = [''] + formatter.formats[:-1]
     formatter.zero_formats[3] = '%b %d'
-    formatter.offset_formats = [''] * 5
-    ax.xaxis.set_major_locator(locator)
+    formatter.offset_formats = [''] * 6
     ax.xaxis.set_major_formatter(formatter)
 
-    # plt.ylabel('руб')
-    # plt.title(f'Динамика изменения цены')
     graph_path = os.path.join(path, f'{spare}.png')
     fig.savefig(graph_path)
     return graph_path
