c33d9962e95cc88351be9777be01a43e3cf31519
Dmitry <da-boyko@mail.ru>
2024-05-01 12:45:10 +0700

    style: add suppressions

--- autos/tasks.py
+++ autos/tasks.py
@@ -5,11 +5,11 @@ from celery_app import app
 
 
 @app.task(bind=True, max_retries=3)
-def do_make_request(self, user: str) -> None:
+def do_make_request(self, user: str) -> None:  # noqa: ANN001
     """Выполнения новых запросов для всех запчастей."""
     try:
         make_request(user)
-    except Exception as err:
+    except Exception as err:  # noqa: BLE001
         self.retry(exc=err, countdown=10)
     make_graphs_for_all_spares()
 
@@ -19,5 +19,3 @@ def do_add_spare(user: str, ad_url: str, ex_url: str, car: int) -> None:
     """Выполнение запроса для одной запчасти."""
     spare = add_spare(user, ad_url, ex_url, car)
     make_graph_for_spare(spare.pk)
-
-# docker-compose run --rm web-app sh -c "python3 manage.py dumpdata --exclude auth.permission --exclude contenttypes > db.json"
