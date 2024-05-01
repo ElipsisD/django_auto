63e7d8faacbfc0d44b3f15b975e4329167609e19
Dmitry <da-boyko@mail.ru>
2024-05-01 12:46:29 +0700

    feat: change parsing service for work with requests

--- autos/services/price_parsing/domain/autodoc_parsing.py
+++ autos/services/price_parsing/domain/autodoc_parsing.py
@@ -1,79 +1,47 @@
 """Парсинг цен на сайте Autodoc.ru."""
-import os
-import re
-from time import sleep
 
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.common import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+import requests
 
-from autos.services.price_parsing.domain.parsing_service import ParsingService, SpareInfo
+from autos.services.price_parsing.domain.parsing_service import TIMEOUT, ParsingService, SpareInfo
+from autoworld.env_settings import AUTODOC_LOGIN, AUTODOC_PASSWORD
 
 
 class AutoDocParsingService(ParsingService):
     """Парсинг данных о запчастях с сайта Autodoc.ru по списку ссылок с помощью функции parse."""
 
-    @staticmethod
-    def _auth(browser: webdriver) -> None:
-        """Авторизация на сайте."""
-        browser.get("https://www.autodoc.ru/")
-        sleep(2)
-        browser.find_element(by=By.XPATH, value='//*[@id="loginInfo"]/div/a').click()
-        sleep(2)
-        browser.find_element(by=By.XPATH, value='//*[@id="Login"]') \
-            .send_keys(os.getenv("AUTODOC_LOGIN"))  # вставляем логин
-        browser.find_element(by=By.XPATH, value='//*[@id="Password"]') \
-            .send_keys(os.getenv("AUTODOC_PASSWORD"))  # вставляем пароль
-        sleep(3)
-        browser.find_element(by=By.XPATH, value='//*[@id="submit_logon_page"]').click()
-
-    @staticmethod
-    def _detail_parsing(page: str) -> SpareInfo | None:
-        """Парсинг данных конкретной запчасти."""
-        soup = BeautifulSoup(page, "lxml")
-        try:
-            tmp = soup.find("h3", string=re.compile("Все предложения запрошенного номера")) \
-                .find_parent("app-price-table")
-            manufacturer = tmp.find("a", class_="company_info_link").text
-            name = tmp.find("div", class_="title-name").text
-            price = int(tmp.find("td", class_="price").find("span").text.split(".")[0].replace(" ", ""))
-            partnumber = tmp.find("div", class_="title-part").text.replace(manufacturer, "").replace(" ", "")
-            delivery_time = int(tmp.find("td", class_="delivery").find("span").text)
-            provider = tmp.find("span", class_="direction-mob direction").text
-        except AttributeError:
-            return None
-        return SpareInfo(name=name,
-                         manufacturer=manufacturer,
-                         price=price,
-                         partnumber=partnumber,
-                         delivery_time=delivery_time,
-                         provider=provider)
-
     @classmethod
     def parse(cls, urls: list) -> dict[str, SpareInfo]:
         """Запуск парсинга данных о запчастях согласно списку, возвращает: {ссылка: данные}."""
-        browser = cls._make_service()
         try:
-            cls._auth(browser)
-            sleep(2)
+            data = {
+                "username": AUTODOC_LOGIN,
+                "password": AUTODOC_PASSWORD,
+                "grant_type": "password"
+            }
+            response = requests.post(
+                "https://auth.autodoc.ru/token",
+                data=data,
+                timeout=TIMEOUT,
+            )
+            response_json = response.json()
+            headers = {"Authorization": f"Bearer {response_json["access_token"]}"}
             res = {}
             for url in urls:
-                browser.get(url)
-                for _ in range(3):
-                    try:
-                        WebDriverWait(browser, timeout=15).until(
-                            lambda x: x.find_element(by=By.TAG_NAME, value="tbody"))
-                        break
-                    except TimeoutException:
-                        browser.get(url)
-                if data := cls._detail_parsing(browser.page_source):
-                    res[url] = data
-                sleep(1)
+                spare_credential = url.removeprefix("https://www.autodoc.ru/price/")
+                spare_response = requests.get(
+                    f"https://webapi.autodoc.ru/api/spareparts/{spare_credential}/3",
+                    headers=headers,
+                    timeout=TIMEOUT,
+                ).json()
+                data = SpareInfo(
+                    name=spare_response["name"],
+                    manufacturer=spare_response["manufacturer"]["name"],
+                    price=int(spare_response["inventoryItems"][0]["price"]),
+                    partnumber=spare_response["partNumber"],
+                    delivery_time=spare_response["inventoryItems"][0]["deliveryDays"],
+                )
+                res[url] = data
+        except Exception as e:  # noqa: BLE001
+            print(e)  # noqa: T201
+        else:
             return res
-        except Exception:
-            pass
-        finally:
-            browser.close()
-            browser.quit()
